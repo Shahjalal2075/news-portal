468b0591ed632cce29eca423a18c1c2738a33b30
Shahjalal2075 <shahjalal2075@gmail.com>
2024-03-10 16:54:01 +0600

    latest news

--- src/Layouts/View/SharedPage/RightSlider/RightSlider.jsx
+++ src/Layouts/View/SharedPage/RightSlider/RightSlider.jsx
@@ -7,7 +7,7 @@ const RightSlider = () => {
                     <img src="https://i.ibb.co/YPNL48R/ad-300x250.jpg" alt="" />
                 </div>
             </div>
-            <div className="bg-[#F8F8F8] mt-10">
+            <div className="bg-[#F8F8F8] mt-10 p-4">
                 <div className="bg-[#DA0000] text-[#fff] font-medium text-lg py-1 ">
                     <h2 className="text-center">সর্বশেষ</h2>
                 </div>
