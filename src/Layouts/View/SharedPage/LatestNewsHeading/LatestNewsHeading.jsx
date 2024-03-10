8801b4112b465abdd166a07ec0d74efee268a543
Shahjalal2075 <shahjalal2075@gmail.com>
2024-03-10 14:20:25 +0600

    top news section

--- src/Layouts/View/SharedPage/LatestNewsHeading/LatestNewsHeading.jsx
+++ src/Layouts/View/SharedPage/LatestNewsHeading/LatestNewsHeading.jsx
@@ -4,8 +4,8 @@ const LatestNewsHeading = () => {
         <div>
             <div className="flex items-center">
                 <h2 className="bg-[#1D1D1D] text-[#fff] px-8 py-3 text-base font-medium">সর্বশেষ</h2>
-                <marquee className="bg-[#FAFAFA] text-[#999] text-base px-8 py-3" direction="left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla eum quia
-                    error placeat recusandae hic, nostrum doloremque nihil ducimus repellendus ea eius architecto doloribus sed.
+                <marquee className="bg-[#FAFAFA] text-[#999] text-base px-8 py-3" direction="left">
+                    এক মাস পর হিলি স্থলবন্দর দিয়ে আবারও আলু আমদানি শুরু এক মাস পর হিলি স্থলবন্দর দিয়ে আবারও আলু আমদানি শুরু এক মাস পর হিলি স্থলবন্দর দিয়ে আবারও আলু আমদানি শুরু এক মাস পর হিলি স্থলবন্দর দিয়ে আবারও আলু আমদানি শুরু এক মাস পর হিলি স্থলবন্দর দিয়ে আবারও আলু আমদানি শুরু
                 </marquee>
             </div>
         </div>
